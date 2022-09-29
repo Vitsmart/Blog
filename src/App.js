360d7e3b6d75b085db1910c371c6c94e9d779829
vitsmart <vitsmart2@gmail.com>
2022-09-29 10:35:05 +0100

    settings update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navigation from "./components/Navigation/Navigation";
+import Login from "./pages/Login/Login";
+//import Settings from "./settings/Settings";
 //import Single from "./pages/Single/Single";
-import Write from "./pages/Write/Write";
+//import Write from "./pages/Write/Write";
 
 function App() {
   return (
     <div>
         <Navigation />
-        <Write />
+        <Login />
     </div>
   )
 }
---
+++ src/settings/Settings.jsx
@@ -0,0 +1,36 @@
+import SideBar from '../SideBar/SideBar'
+import './Settings.css'
+
+export default function Settings() {
+  return (
+    <div className='settings'>
+        <div className="settingsWrapper">
+            <div className="settingsTitle">
+                <span className="settingsUpdateTitle">Update Your Account</span>
+                <span className="settingsDeleteTitle">Delete Account</span>
+            </div>
+            <form className="settingsForm">
+                <label>Profile picture</label>
+                <div className="settingsPP">
+                    <img
+                    src='https://guardian.ng/wp-content/uploads/life/2015/08/jideofstola_2015-07-12_18-23-39.jpg'
+                    alt='settingsimg'
+                    />
+                    <label htmlFor='fileInput'>
+                    <i className="settingsPPIcon fa-solid fa-circle-user"></i>
+                    </label>
+                    <input type="file" id='fileInput' style={{display: "none"}} />
+                </div>
+                <label>Username</label>
+                <input type="text" placeholder='Vitman'/>
+                <label>Email</label>
+                <input type="email" placeholder='Vitman@gmail.com'/>
+                <label>Password</label>
+                <input type="password" />
+                <button className='settingsSubmit'>Update</button>
+            </form>
+        </div>
+        <SideBar />
+    </div>
+  )
+}
