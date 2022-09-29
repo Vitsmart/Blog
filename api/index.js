f198a317a564fcd418a7d5512fe41fedfabd8835
vitsmart <vitsmart2@gmail.com>
2022-09-29 19:18:06 +0100

    mongo set-up

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTypology: true,
+    useCreateIndex: true,
+}).then(console.log("connected to Mongo db")).catch((err) => console.log(err));
 app.listen("5000", () => {
 console.log("Backend is running");
 })
