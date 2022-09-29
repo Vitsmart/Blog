67d239cefe0ea36a4731ce987eecc828692006c8
vitsmart <vitsmart2@gmail.com>
2022-09-29 13:25:35 +0100

    routing pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import Navigation from "./components/Navigation/Navigation";
-//import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-//import Settings from "./settings/Settings";
-//import Single from "./pages/Single/Single";
-//import Write from "./pages/Write/Write";
+import Settings from "./settings/Settings";
+import Single from "./pages/Single/Single";
+import Write from "./pages/Write/Write";
+import {BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home";
+import Login from "./pages/Login/Login";
 
 function App() {
+
+const user = true;
+
   return (
-    <div>
-        <Navigation />
-        <Register />
-    </div>
+    <>
+    <Navigation />
+   <BrowserRouter>  
+    <Routes>
+     <Route path='/' element={<Home />} />
+     <Route path='register' element=
+     {user ? <Home/> : <Register />} />
+     <Route path='login' element={user ? <Home/> :<Login/>} />
+     <Route path='write' element={user ? <Home/> :<Write/>} />
+     <Route path='settings' element={user ? <Home/> :<Settings />} />
+     <Route path='post/:postId' element={<Single />} />
+
+    
+    </Routes>
+    </BrowserRouter>
+    </>
+  
   )
 }
 
