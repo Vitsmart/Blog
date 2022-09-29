96bfb61186f7e0ef17a24fdd29cfccb16acfb845
vitsmart <vitsmart2@gmail.com>
2022-09-29 12:44:09 +0100

    registration page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navigation from "./components/Navigation/Navigation";
-import Login from "./pages/Login/Login";
+//import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 //import Settings from "./settings/Settings";
 //import Single from "./pages/Single/Single";
 //import Write from "./pages/Write/Write";
@@ -8,7 +9,7 @@ function App() {
   return (
     <div>
         <Navigation />
-        <Login />
+        <Register />
     </div>
   )
 }
