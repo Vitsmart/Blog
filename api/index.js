fd6cd24cdec07dbdbfe84fcaa59f3e2adef5e233
vitsmart <vitsmart2@gmail.com>
2022-09-29 23:15:32 +0100

    postman set-up

--- api/index.js
+++ api/index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose")
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+
+app.use(express.json());
+
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTypology: true,
     useCreateIndex: true,
 }).then(console.log("connected to Mongo db")).catch((err) => console.log(err));
+
+
+app.use("/api/auth", authRoute);
+
+
 app.listen("5000", () => {
 console.log("Backend is running");
 })
+
+module.exports = router
