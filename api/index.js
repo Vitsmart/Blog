21ac6c301fa5812c7b5c31e895c99a77bfa95c43
vitsmart <vitsmart2@gmail.com>
2022-09-30 16:36:52 +0100

    all cat

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./post/users");
 const categoryRoute = require("./post/categories");
+const multer = require("multer")
 
 app.use(express.json());
 
@@ -16,6 +17,18 @@ mongoose.connect(process.env.MONGO_URL,{
     useCreateIndex: true,
 }).then(console.log("connected to Mongo db")).catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
+const upload = multer({storage: storage});
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
