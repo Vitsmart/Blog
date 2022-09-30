680feacbf815409c8b4c3f5e84378c37acc710e6
vitsmart <vitsmart2@gmail.com>
2022-09-30 15:57:45 +0100

    getPost

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -42,6 +42,55 @@ try{
 }),
 
 //====== Delete post =========
+router.delete("/:id", async (req, res) => {
+    try{
+const post = await Post.findById(req.params.id);
+if(post.username === req.body.username)
+
+try{
+   await post.delete()
+    res.status(200).json("post has been deleted...");
+}catch(err){
+    res.status(500).json(err)
+} else{
+    res.status(401).json("You can delete only your post!")
+}
+    }catch(err){
+        res.status(500).json(err);
+    }
+}),
+
+// Get post ===================
+router.get("/:id", async (req, res) => {
+    try{
+const post = await Post.findById(req.params.id);
+res.status(200).json(post);
+
+}catch(err){
+    res.status(500).json(err)
+    }
+}),
+//  getting All post ======
+router.get("/", async (req, res) => {
+
+const username = req.query.user;
+const catName = req.query.cat;
+
+    try{
+        let posts;
+        if (username){
+            posts = await Post.find({username})
+        } else if(catName){
+            posts = await Post.find({categories:{$in:[catName]}})
+        } else{
+            posts = await Post.find();
+        }
+res.status(200).json(posts);
+
+}catch(err){
+    res.status(500).json(err)
+    }
+}),
 
 
 
