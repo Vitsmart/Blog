716fcec154c36b8e21c86e3e3db86e4ec15aebac
vitsmart <vitsmart2@gmail.com>
2022-09-30 15:27:44 +0100

    updatedPost

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -5,9 +5,9 @@ const Post = require("../models/Post");
 
 //====== Create new post==========
 router.post("/", async (req, res) => {
-    const newPost = new Post(req.body);
+    const newPost = new Post(req.body)
     try{
-const savedPost = newPost.save();
+const savedPost = await newPost.save();
 res.status(200).json(savedPost);
     }catch(err){
         res.status(500).json(err)
@@ -15,40 +15,34 @@ res.status(200).json(savedPost);
 }),
 //======= Update post =====
 
-router.delete("/:id", async (req, res) => {
-    if (req.body.userId === req.params.id){
-       
-        try {
-
-const user = await User.findById(req.params.id);
-      
-
+router.put("/:id", async (req, res) => {
     try{
-        await Post.deleteMany({username: user.username})
-        await User.findByIdAndDelete(req.params.id)
-res.status(200).json("user has been deleted...");
+const post = await Post.findById(req.params.id);
+if(post.username === req.body.username)
+
+try{
+    const updatedPost = await Post.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set:req.body,
+        },
+        {new: true}
+    );
+    res.status(200).json(updatedPost);
+}catch(err){
+    res.status(500).json(err)
+} else{
+    res.status(401).json("You can update only your post!")
+}
+
+
     }catch(err){
         res.status(500).json(err);
     }
-
-} catch(err) {
-    res.status(404).json("user not found")
-}
-} else {
-    res.status(401).json("You can delete only your account")
-}
 }),
 
-//====== Get User =========
+//====== Delete post =========
 
-router.get("/:id", async (req, res) => {
-try{
-const user = await User.findById(req.params.id);
-const {password, ...others} = user._doc;
-res.status(200).json(others);
-}catch(err){
-    res.status(500).json(err)
-}
-});
+
 
 module.exports = router;
