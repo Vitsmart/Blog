612fb7434e7eb7da5506dd9406557e2449e291c1
vitsmart <vitsmart2@gmail.com>
2022-09-30 14:51:16 +0100

    users

--- api/index.js
+++ api/index.js
@@ -3,22 +3,23 @@ const app = express();
 const mongoose = require("mongoose")
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 app.use(express.json());
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser: true,
-    useUnifiedTypology: true,
+     useNewUrlParser: true,
+     useUnifiedTypology: true,
     useCreateIndex: true,
 }).then(console.log("connected to Mongo db")).catch((err) => console.log(err));
 
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 
 app.listen("5000", () => {
-console.log("Backend is running well");
+console.log("Backend is running on port 5000");
 })
 
-module.exports = router
