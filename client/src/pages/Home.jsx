2e9a1dd99036a2e46d67bce2e872668e209d29a0
vitsmart <vitsmart2@gmail.com>
2022-10-05 18:12:44 +0100

    starting connection

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/header/Header'
 import Posts from '../Posts/Posts'
 import SideBar from '../SideBar/SideBar'
 import './Home.css'
 
 export default function Home() {
+const [posts, setPosts] = useState([]);
+
+useEffect(() => {
+  const fetchPosts = async () =>{
+    const res = axios.get("/posts")
+    console.log(res)
+  }
+  fetchPosts();
+},[])
+
   return (
     <>
         <Header />
