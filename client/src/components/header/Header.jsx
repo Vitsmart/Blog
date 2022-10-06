7fc8336a2457168b910f96aab4327f646c74ea8a
vitsmart <vitsmart2@gmail.com>
2022-10-06 22:30:09 +0100

    updated landing page

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className='header'>
         <div className="headerTitles">
-            <span className='headerTitleSm'>React & Node</span>
+            <span className='headerTitleSm'>Noble Express</span>
             <span className='headerTitleLg'>Blog</span>
         </div>
         <img className='headerTitleImg' src='https://wallpaper.dog/large/994212.jpg' alt='' />
